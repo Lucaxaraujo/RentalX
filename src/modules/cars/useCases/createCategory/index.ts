f3b7f09de15169bfce5a443f4cb01f51ee088654
Lucaxaraujo <lucasm030@gmail.com>
2021-10-16 18:25:53 -0300

    Creating category Use Case

---
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -0,0 +1,14 @@
+// arquivo usado para isolar as instancias que precisarão ser criadas para serem usadas nas rotas
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { CreateCategoryController } from "./CreateCategoryController";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+const categoriesRepository = new CategoriesRepository();
+
+const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+
+const createCategoryController = new CreateCategoryController(
+  createCategoryUseCase
+);
+
+export { createCategoryController };
