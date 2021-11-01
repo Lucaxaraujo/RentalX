b7b2473958ee7f2ab0a9ac0561f481cf98ed9819
lucasmoreiraiclubs <lucas.moreira@iclubs.com.br>
2021-10-31 21:23:40 -0300

    Isolating interfaces implementations

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ListCategoriesContoller } from "./ListCategoriesContoller";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
